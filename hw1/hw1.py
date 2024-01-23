b15c8d639fcf8b21d026ca6ab707f4458c91b4d7
Evan Klepitsch <e.klepitsch@hotmail.com>
2024-01-23 08:43:54 -0700

    First working version of HW1

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -47,35 +47,35 @@ print(f'Value in center frequency bin: {center_frequency_bin_value}')
 
 # Make some plots of the averaged FFT in each direction, at the point of the
 # center frequency.  This will show us the noise profile in each direction.
-# fig_noise, ax_noise = plt.subplots(1, 3)
-# ax_noise[0].plot(k, f_hat_avg[:, center_frequency_bin[1],
-#                  center_frequency_bin[2]], linewidth=3)
-# ax_noise[0].axvline(center_frequency_bin[0], color='r', linestyle='--',
-#                     label=f'Center frequency = {center_frequency_bin[0]}')
-# ax_noise[0].set_title(r'$\bf{x}$-direction')
-# ax_noise[0].set_xlabel('Frequency bin (k)')
-# ax_noise[0].set_ylabel('Magnitude')
-# ax_noise[0].legend()
-# ax_noise[1].plot(k, f_hat_avg[center_frequency_bin[0], :,
-#                  center_frequency_bin[2]], linewidth=3)
-# ax_noise[1].axvline(center_frequency_bin[1], color='r', linestyle='--',
-#                     label=f'Center frequency = {center_frequency_bin[1]}')
-# ax_noise[1].set_title(r'$\bf{y}$-direction')
-# ax_noise[1].set_xlabel('Frequency bin (k)')
-# ax_noise[1].set_ylabel('Magnitude')
-# ax_noise[1].legend()
-# ax_noise[2].plot(k, f_hat_avg[center_frequency_bin[0],
-#                  center_frequency_bin[1], :], linewidth=3)
-# ax_noise[2].axvline(center_frequency_bin[2], color='r', linestyle='--',
-#                     label=f'Center frequency = {center_frequency_bin[2]}')
-# ax_noise[2].set_title(r'$\bf{z}$-direction')
-# ax_noise[2].set_xlabel('Frequency bin (k)')
-# ax_noise[2].set_ylabel('Magnitude')
-# ax_noise[2].legend()
-# fig_noise.set_figwidth(15)
-# fig_noise.tight_layout(pad=3)
-# fig_noise.suptitle('Time averaged FFT centered at the submarine\'s frequency')
-# fig_noise.show()
+fig_noise, ax_noise = plt.subplots(1, 3)
+ax_noise[0].plot(k, f_hat_avg[:, center_frequency_bin[1],
+                 center_frequency_bin[2]], linewidth=3)
+ax_noise[0].axvline(center_frequency_bin[0], color='r', linestyle='--',
+                    label=f'Center frequency = {center_frequency_bin[0]}')
+ax_noise[0].set_title(r'$\bf{x}$-direction')
+ax_noise[0].set_xlabel('Frequency bin (k)')
+ax_noise[0].set_ylabel('Magnitude')
+ax_noise[0].legend()
+ax_noise[1].plot(k, f_hat_avg[center_frequency_bin[0], :,
+                 center_frequency_bin[2]], linewidth=3)
+ax_noise[1].axvline(center_frequency_bin[1], color='r', linestyle='--',
+                    label=f'Center frequency = {center_frequency_bin[1]}')
+ax_noise[1].set_title(r'$\bf{y}$-direction')
+ax_noise[1].set_xlabel('Frequency bin (k)')
+ax_noise[1].set_ylabel('Magnitude')
+ax_noise[1].legend()
+ax_noise[2].plot(k, f_hat_avg[center_frequency_bin[0],
+                 center_frequency_bin[1], :], linewidth=3)
+ax_noise[2].axvline(center_frequency_bin[2], color='r', linestyle='--',
+                    label=f'Center frequency = {center_frequency_bin[2]}')
+ax_noise[2].set_title(r'$\bf{z}$-direction')
+ax_noise[2].set_xlabel('Frequency bin (k)')
+ax_noise[2].set_ylabel('Magnitude')
+ax_noise[2].legend()
+fig_noise.set_figwidth(15)
+fig_noise.tight_layout(pad=3)
+fig_noise.suptitle('Time averaged FFT centered at the submarine\'s frequency')
+fig_noise.show()
 
 
 # Define a Gaussian filter
@@ -83,51 +83,45 @@ def gaussian(x, center=0, sigma=1, scale=1):
     return scale * np.exp(-(x - center)**2/sigma**2)
 
 
-def gaussian_3d(x, y, z, center=(0, 0, 0), sigma=(0, 0, 0), scale=1):
-    return scale * np.exp(-(((x - center[0])**2)/(sigma[0]**2) +
-                            ((y - center[1])**2)/(sigma[1]**2) +
-                            ((z - center[2])**2)/(sigma[2]**2)))
-
-
 # Make some plots to show the filter design in each direction
-# fig_filter, ax_filter = plt.subplots(1, 3)
-# ax_filter[0].plot(k, f_hat_avg[:, center_frequency_bin[1],
-#                     center_frequency_bin[2]], linewidth=3)
-# ax_filter[0].set_title(r'$\bf{x}$-direction')
-# ax_filter[0].set_xlabel('Frequency bin (k)')
-# ax_filter[0].set_ylabel('Magnitude')
-# ax_filter[1].plot(k, f_hat_avg[center_frequency_bin[0], :,
-#                     center_frequency_bin[2]], linewidth=3)
-# ax_filter[1].set_title(r'$\bf{y}$-direction')
-# ax_filter[1].set_xlabel('Frequency bin (k)')
-# ax_filter[1].set_ylabel('Magnitude')
-# ax_filter[2].plot(k, f_hat_avg[center_frequency_bin[0],
-#                     center_frequency_bin[1], :], linewidth=3)
-# ax_filter[2].set_title(r'$\bf{z}$-direction')
-# ax_filter[2].set_xlabel('Frequency bin (k)')
-# ax_filter[2].set_ylabel('Magnitude')
-# fig_filter.set_figwidth(15)
-# fig_filter.tight_layout(pad=3)
-# fig_filter.suptitle('Gaussian filter design in each direction')
-#
-# for sigma in range(1, 20):
-#     filter_x = gaussian(k, center_frequency_bin[0], sigma,
-#                         center_frequency_bin_value)
-#     filter_y = gaussian(k, center_frequency_bin[1], sigma,
-#                         center_frequency_bin_value)
-#     filter_z = gaussian(k, center_frequency_bin[2], sigma,
-#                         center_frequency_bin_value)
-#     if sigma in [1, 3, 10]:
-#         ax_filter[0].plot(k, filter_x, label=rf'$\sigma$ = {sigma}', alpha=0.75, linestyle='--')
-#     if sigma in [1, 5, 10]:
-#         ax_filter[1].plot(k, filter_y, label=rf'$\sigma$ = {sigma}', alpha=0.75, linestyle='--')
-#     if sigma in [1, 3, 10]:
-#         ax_filter[2].plot(k, filter_z, label=rf'$\sigma$ = {sigma}', alpha=0.75, linestyle='--')
-#
-# ax_filter[0].legend()
-# ax_filter[1].legend()
-# ax_filter[2].legend()
-# fig_filter.show()
+fig_filter, ax_filter = plt.subplots(1, 3)
+ax_filter[0].plot(k, f_hat_avg[:, center_frequency_bin[1],
+                    center_frequency_bin[2]], linewidth=3)
+ax_filter[0].set_title(r'$\bf{x}$-direction')
+ax_filter[0].set_xlabel('Frequency bin (k)')
+ax_filter[0].set_ylabel('Magnitude')
+ax_filter[1].plot(k, f_hat_avg[center_frequency_bin[0], :,
+                    center_frequency_bin[2]], linewidth=3)
+ax_filter[1].set_title(r'$\bf{y}$-direction')
+ax_filter[1].set_xlabel('Frequency bin (k)')
+ax_filter[1].set_ylabel('Magnitude')
+ax_filter[2].plot(k, f_hat_avg[center_frequency_bin[0],
+                    center_frequency_bin[1], :], linewidth=3)
+ax_filter[2].set_title(r'$\bf{z}$-direction')
+ax_filter[2].set_xlabel('Frequency bin (k)')
+ax_filter[2].set_ylabel('Magnitude')
+fig_filter.set_figwidth(15)
+fig_filter.tight_layout(pad=3)
+fig_filter.suptitle('Gaussian filter design in each direction')
+
+for sigma in range(1, 20):
+    filter_x = gaussian(k, center_frequency_bin[0], sigma,
+                        center_frequency_bin_value)
+    filter_y = gaussian(k, center_frequency_bin[1], sigma,
+                        center_frequency_bin_value)
+    filter_z = gaussian(k, center_frequency_bin[2], sigma,
+                        center_frequency_bin_value)
+    if sigma in [1, 3, 10]:
+        ax_filter[0].plot(k, filter_x, label=rf'$\sigma$ = {sigma}', alpha=0.75, linestyle='--')
+    if sigma in [1, 5, 10]:
+        ax_filter[1].plot(k, filter_y, label=rf'$\sigma$ = {sigma}', alpha=0.75, linestyle='--')
+    if sigma in [1, 3, 10]:
+        ax_filter[2].plot(k, filter_z, label=rf'$\sigma$ = {sigma}', alpha=0.75, linestyle='--')
+
+ax_filter[0].legend()
+ax_filter[1].legend()
+ax_filter[2].legend()
+fig_filter.show()
 
 # In x direction, choose sigma = 3.
 # In y direction, choose sigma = 5.
@@ -180,4 +174,19 @@ fig_position.suptitle('Position of submarine')
 fig_position.show()
 
 # Now plot the 3-D path
+fig_position_3d, ax_position_3d = plt.subplots(subplot_kw=dict(projection='3d'))
+ax_position_3d.scatter3D(submarine_position[0, 0], submarine_position[0, 1],
+                         submarine_position[0, 2], color='k', label='t = 0 hrs')  # First point
+# Points 0 and 1 are the same, so skip point 1
+ax_position_3d.scatter3D(submarine_position[2:-1, 0], submarine_position[2:-1, 1],
+                         submarine_position[2:-1, 2])  # Middle points
+ax_position_3d.scatter3D(submarine_position[-1, 0], submarine_position[-1, 1],
+                         submarine_position[-1, 2], color='r', label='t = 24 hrs')  # Last point
+ax_position_3d.view_init(30, 150)
+ax_position_3d.set_xlabel('x')
+ax_position_3d.set_ylabel('y')
+ax_position_3d.set_zlabel('z')
+ax_position_3d.legend()
+fig_position_3d.suptitle('Position of submarine in 3-D')
+fig_position_3d.show()
 
